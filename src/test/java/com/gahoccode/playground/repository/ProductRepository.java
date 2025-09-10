d4797c71ad74f34430d424a92963ce7b03b82388
dominhit215@gmail.com <dominhit215@gmail.com>
2025-09-10 22:37:02 +0700

    sort pagination test

--- src/test/java/com/gahoccode/playground/repository/ProductRepository.java
+++ src/test/java/com/gahoccode/playground/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.gahoccode.playground.repository;
 
 import com.gahoccode.playground.entity.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.SearchPage;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +16,15 @@ public interface ProductRepository extends ElasticsearchRepository<Product, Inte
     SearchHits<Product> findByCategory(String category);
 
     SearchHits<Product> findByCategoryIn(List<String> categories);
+
+    SearchHits<Product> findByCategoryAndBrand(String category, String brand);
+
+    SearchHits<Product> findByName(String name);
+
+    SearchHits<Product> findByPriceLessThan(Integer price);
+
+    SearchHits<Product> findByPriceBetween(Integer from, Integer to, Sort sort);
+
+    // pagination demo
+    SearchPage<Product> findByCategory(String category, Pageable pageable);
 }
